b730bc17fb2ae94e4301494427a65801d02488d0
tiagoforward <devtiagoforward@gmail.com>
2024-09-15 13:54:08 -0300

    adjusted

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -28,7 +28,7 @@ const config: StorybookConfig = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system-ignite/'
+      config.base = '/designer-system-ignite/'
     }
 
     return config
